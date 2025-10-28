b77a09a646874a0b785d4331e15bc2aa3d936ba9
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-10-28 13:03:30 -0300

    0.0.1: fps counter

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 console.log('Hello world');
 
 // Var list
-let x = 0
+let context = document.getElementById("game").getContext("2d");
+let fps = {shown: 0, count: 0}
 
 // Game start
 function gameStart() {
-
+    context = document.getElementById("game").getContext("2d");
+	
+	setInterval(function() {
+		fps.shown = fps.count;
+        fps.count = 0;
+        console.log(fps.shown);
+	}, 1000);
 }
 function gameLateStart() {
 
@@ -14,7 +21,7 @@ function gameLateStart() {
 // Game update
 function gameUpdate() {
     GameTime.Update();
-    console.log(GameTime.delta);
+    
 }
 function gameLateUpdate() {
 
@@ -25,11 +32,12 @@ function main() {
     gameStart();
     gameLateStart();
 
-    let x = 0;
     const mainLoop = () => {
         gameUpdate();
         gameLateUpdate();
 
+        fps.count++;
+        //console.log(fps.count);
         requestAnimationFrame(mainLoop);
     }
     requestAnimationFrame(mainLoop)
