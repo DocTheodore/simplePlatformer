8929a9f099ad8c49d155a7d9271e59b10a813923
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-12-01 18:00:16 -0300

    0.0.1: entity class

---
+++ src/shared/class/entity.ts
@@ -0,0 +1,81 @@
+import { EntityId } from "../types/components";
+
+export type ComponentsMap = Record<string, any>;
+
+export class Entities {
+    private entities = new Map<EntityId, ComponentsMap>();
+
+    create(id:EntityId) {
+        this.entities.set(id, {});
+        return id;
+    }
+
+    destroy(id:EntityId) {
+        this.entities.delete(id);
+        return id;
+    }
+
+    add<T>(id:EntityId, type:string, component: T) {
+        const ent = this.entities.get(id);
+        if (ent) ent[type] = component;
+    }
+
+    remove(id:EntityId, type:string) {
+        const ent = this.entities.get(id);
+        if (ent) delete ent[type];
+    }
+
+    get<T>(id:EntityId, type:string): T | undefined {
+        return this.entities.get(id)?.[type];
+    }
+
+    has(id:EntityId, ...types:string[]): boolean {
+        const ent = this.entities.get(id);
+        if (!ent) return false;
+        return types.every(t => t in ent);
+    }
+
+    query(...types:string[]): [EntityId, ComponentsMap][] {
+        const result: [EntityId, ComponentsMap][] = [];
+
+        for(const [id, comps] of this.entities.entries()) {
+            if(types.every(t => t in comps)) {
+                result.push([id, comps]);
+            }
+        }
+
+        return result;
+    }
+
+    getDelta(previous: Map<EntityId, ComponentsMap>): any[] {
+        const delta:any[] = []
+
+        for(const [id, current] of this.entities ) {
+            const prev = previous.get(id) || {};
+            const changes:any = {};
+
+            for (const key in current) {
+                if(JSON.stringify(current[key]) !== JSON.stringify(prev[key])) {
+                    changes[key] = current[key];
+                }
+            }
+
+            if(Object.keys(changes).length > 0 || !previous.has(id)) {
+                delta.push({id, ...changes});
+            }
+
+        }
+
+        for(const id of previous.keys()){
+            if(!this.entities.has(id)) {
+                delta.push({ id, $removed: true});
+            }
+        }
+
+        return delta;
+    }
+
+    snapshot() {
+        return new Map(this.entities);
+    }
+}
