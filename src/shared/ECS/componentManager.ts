e671c5ecb8a6cd799d090fe55030bc1e9703c8af
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-12-12 17:01:13 -0300

    0.0.1: componentManager and entityManager

---
+++ src/shared/ECS/componentManager.ts
@@ -0,0 +1,46 @@
+import { ComponentStore } from "./components/componentStore";
+
+export class ComponentManager {
+    private stores = new Map<number, ComponentStore>();
+    private entityMasks:number[] = [];
+
+    registerComponent(componentId: number, store: ComponentStore) {
+        this.stores.set(componentId, store);
+    }
+
+    addComponent(componentId: number, entity: number) {
+        const store = this.stores.get(componentId);
+        if (!store) throw 'Componente não registrado';
+
+        store.add(entity);
+
+        this.entityMasks[entity] = (this.entityMasks[entity] ?? 0) | componentId;
+    }
+
+    removeComponent(componentId: number, entity: number) {
+        const store = this.stores.get(componentId);
+        if (!store) return;
+
+        if(store.has(entity)) {
+            store.remove(entity);
+            this.entityMasks[entity] &= ~componentId;
+        }
+    }
+
+    hasComponent(componentId: number, entity: number): boolean {
+        return (this.entityMasks[entity] & componentId) !== 0;
+    }
+
+    getStore<T extends ComponentStore>(componentId: number): T {
+        return this.stores.get(componentId) as T;
+    }
+
+    destroyEntity(entity: number) {
+        for (const [componentId, store] of this.stores) {
+            if(store.has(entity)) {
+                store.remove(entity);
+                this.entityMasks[entity] &= ~componentId;
+            }
+        }
+    }
+}
